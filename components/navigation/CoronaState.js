c4be0a34826b325c10f0b70dd290d5351651cc22
unknown <bu8886679@BSVDI.COM>
2020-03-23 18:00:57 +0900

    수정중

--- components/navigation/CoronaState.js
+++ components/navigation/CoronaState.js
@@ -3,19 +3,41 @@ import { View, Text, StyleSheet } from 'react-native';
 
 export default class CoronaState extends Component {
 
+    state = {
+
+    }
+
     UNSAFE_componentWillMount(){
         this.fetchCoronaState();
     }
 
     fetchCoronaState = () => {
-        fetch('//api.dropper.tech/covid19/status/korea')
-        .then(res => console.log(res))
+        var myHeaders = new Headers();
+        myHeaders.append("APIKey", "27741b4d46a7a2657517342d05150e2ccbdf78d5174daabafc9ebcb9dbe45e59");
+
+        var requestOptions = {
+            method: 'GET',
+            headers: myHeaders,
+            redirect: 'follow'
+        };
+
+        fetch("https://api.dropper.tech/covid19/status/korea?locale=busan", requestOptions)
+        .then(res=> {
+            this.setState({
+                res
+            })
+        })
+        .catch(error => console.log('error', error));
     }
 
     render(){
         return (
             <View style={style.container}>
-                <Text>CoronaState</Text>
+                {
+                    this.state.data ?
+                    <Text>{this.state.data}</Text>
+                    : null
+                }
             </View>
         );
     }
